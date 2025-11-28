39c43281934a0ed6841e0c3a3095474c92f02cca
nazif.sk-peermahomed <nazif.sk-peermahomed@epita.fr>
2025-11-28 09:43:15 +0100

    project init

---
+++ src/main/java/com/example/projet_skpeermahomed_nazif/ProjetSkPeermahomedNazifApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_skpeermahomed_nazif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetSkPeermahomedNazifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetSkPeermahomedNazifApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projet_skpeermahomed_nazif/ProjetSkPeermahomedNazifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_skpeermahomed_nazif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSkPeermahomedNazifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
